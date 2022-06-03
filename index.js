036ee889ca37ff58fd05ca79afd23793e55878ed
dungaaaa <86231982+dungaaaa@users.noreply.github.com>
2022-06-03 23:43:50 +0700

    coomit

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ const server = app.listen(process.env.PORT, () => {
 
 const io = socket(server, {
   cors: {
-    origin: ["http://localhost:3000"],
+    origin: ["http://localhost:3000", "https://apptangnguoiyeu.netlify.app"],
     credentials: true,
   },
 });
